871f9e58d4b27dc298be5bc09d25de3d4df023e4
Yeon Gyu Kim <public.kim.yeon.gyu@gmail.com>
2020-03-02 19:07:23 +0900

    Update main.go to use goroutine for async sending

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
@@ -19,6 +20,26 @@ func contains(arr []int64, num int64) bool {
 }
 
 var bot, err = tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_CODE"))
+
+func sendMsg(bot *tgbotapi.BotAPI, text string, receiver int64) (tgbotapi.Message, error) {
+	msg := tgbotapi.NewMessage(receiver, text)
+	return bot.Send(msg)
+}
+
+func sendTillSuccess(wg *sync.WaitGroup, bot *tgbotapi.BotAPI, text string, id int64) {
+	_, err := sendMsg(bot, fmt.Sprintf(text), id)
+	if err != nil {
+		if err.Error() == "Bad Request: chat not found" {
+			RemoveChatID(id)
+			wg.Done()
+			return
+		}
+		wg.Add(1)
+		time.Sleep(time.Second)
+		sendTillSuccess(wg, bot, text, id)
+	}
+	wg.Done()
+}
 
 func runBot() {
 	if err != nil {
@@ -48,20 +69,16 @@ func runBot() {
 			}
 			text := "/help: 명령어 목록\n/current: 현재 정보 받기\n\n"
 			text += "코로나19 관련 데이터 변동시 메시지를 보내드립니다."
-
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, text)
-			bot.Send(msg)
+			sendMsg(bot, text, update.Message.Chat.ID)
 		case "/help":
 			text := "/help: 명령어 목록\n/current: 현재 정보 받기\n\n"
 			text += "소스코드: https://github.com/code-yeongyu/corona19-telegram-notifier-bot"
-
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, text)
-			bot.Send(msg)
+			sendMsg(bot, text, update.Message.Chat.ID)
 		case "/current":
 			numbers := GetRecentNumbers()
 			text := fmt.Sprintf("확진자: %d명\n사망자: %d\n완치자: %d명\n\n현재 감염자: %d명", numbers["confirmed"], numbers["death"], numbers["cured"], numbers["confirmed"]-numbers["death"]-numbers["cured"])
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, text)
-			bot.Send(msg)
+
+			sendMsg(bot, text, update.Message.Chat.ID)
 		}
 	}
 }
@@ -73,6 +90,7 @@ func alertIfDiff() error {
 	recentTotal := 0
 	numbersTotal := 0
 
+	//validate numbers and recent
 	if len(numbers) == 0 || len(recent) == 0 {
 		return fmt.Errorf("something includes 0")
 	}
@@ -92,7 +110,10 @@ func alertIfDiff() error {
 	if recentTotal == numbersTotal {
 		return nil
 	}
+	fmt.Println(numbers) // for monitoring
 	AddNumbers(numbers)
+
+	// put values
 	confirmed := numbers["confirmed"]
 	death := numbers["death"]
 	cured := numbers["cured"]
@@ -100,14 +121,16 @@ func alertIfDiff() error {
 	text := fmt.Sprintf("확진자: %d명\n사망자: %d\n완치자: %d명\n\n현재 감염자: %d명\n\n확진자 증가수: %d명\n사망자 증가수: %d명\n완치자 증가수: %d명",
 		confirmed, death, cured, confirmed-death-cured, confirmed-recent["confirmed"], death-recent["death"], cured-recent["cured"])
 	chatIDs := GetChatIDs()
-	fmt.Println(numbers)
-	for _, chatID := range chatIDs {
-		msg := tgbotapi.NewMessage(chatID, text)
-		_, err = bot.Send(msg)
-		if err != nil {
-			RemoveChatID(chatID)
-		}
+
+	var wg sync.WaitGroup
+	wg.Add(len(chatIDs))
+	startTime := time.Now()
+	for i := range chatIDs {
+		go sendTillSuccess(&wg, bot, text, chatIDs[i])
 	}
+	elapsedTime := time.Since(startTime)
+	wg.Wait()
+	fmt.Printf("%f took to send to %d people.\n", elapsedTime.Seconds(), len(chatIDs))
 	return nil
 }
 
